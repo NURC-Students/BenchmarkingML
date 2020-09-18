552a8170606a7c7b7e9d03fa463f815b24cd4dfb
Srinjoy Chakravarty <chakravarty.s@northeastern.edu>
2020-09-18 04:11:56 -0400

    :black_nib: added daal4py tester code within jupyter notebook

--- MultipleLinearRegression/daal4py/kmeans.py
+++
@@ -1,8 +0,0 @@
-from daal4py import daalinit, daalfini, kmeans_init
-import numpy as np
-
-X = np.array([[1.,1.], [1.,4.], [1.,0.]])
-daalinit()
-result = kmeans_init(10, method="plusPlusDense", distributed=True).compute(X)
-daalfini()
-print(result.centroids)
