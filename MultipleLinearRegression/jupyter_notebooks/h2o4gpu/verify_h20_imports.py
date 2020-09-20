cb254d87237358ecbcbd9bb9b90754e825e8676c
Srinjoy Chakravarty <chakravarty.s@northeastern.edu>
2020-09-20 10:21:40 -0400

    :gear: h204gpu set in motion

---
+++ MultipleLinearRegression/jupyter_notebooks/h2o4gpu/verify_h20_imports.py
@@ -0,0 +1,7 @@
+import h2o4gpu
+import h2o4gpu as sklearn
+from h2o4gpu import DAAL_SUPPORTED
+import matplotlib
+from sklearn import datasets, linear_model
+import matplotlib.pyplot as plt
+import numpy as np
