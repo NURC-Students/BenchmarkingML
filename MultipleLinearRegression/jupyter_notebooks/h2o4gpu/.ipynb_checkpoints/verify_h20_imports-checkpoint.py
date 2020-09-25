be8e0eb4e820c44f071eb627f03559e3a07bafff
Srinjoy Chakravarty <chakravarty.s@northeastern.edu>
2020-09-25 08:37:02 -0400

    :seedling: updated h2o notebook end to end

---
+++ MultipleLinearRegression/jupyter_notebooks/h2o4gpu/.ipynb_checkpoints/verify_h20_imports-checkpoint.py
@@ -0,0 +1,9 @@
+import h2o4gpu
+import h2o4gpu as sklearn
+from h2o4gpu import DAAL_SUPPORTED
+import matplotlib
+from sklearn import datasets, linear_model
+import matplotlib.pyplot as plt
+import numpy as np
+
+print("h2o4gpu, sklearn, matplotlib, numpy imports successful!")
