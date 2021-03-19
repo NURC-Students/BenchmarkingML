f39516dc2c13bfc3566d1e043086227ca9e79357
Abhishek-Gargha-Maheshwarappa <maheshwarappa.a@husky.neu.edu>
2021-03-18 20:33:35 -0400

    updating the empty branch

--- MultipleLinearRegression/jupyter_notebooks/h2o4gpu/.ipynb_checkpoints/verify_h20_imports-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import h2o4gpu
-import h2o4gpu as sklearn
-from h2o4gpu import DAAL_SUPPORTED
-import matplotlib
-from sklearn import datasets, linear_model
-import matplotlib.pyplot as plt
-import numpy as np
-
-print("h2o4gpu, sklearn, matplotlib, numpy imports successful!")
